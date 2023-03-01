5675fbdc47aa16dfadcfd1ec2c84e6297a0321ee
אביטל כהנא <avkahana@g.jct.ac.il>
2023-03-02 00:00:55 +0200

    first step

---
+++ src/primitives/Point.java
@@ -0,0 +1,48 @@
+package primitives;
+
+public class Point {
+    final Double3 xyz;
+
+    Point(Double3 xyz) {
+        this.xyz = xyz;
+    }
+    public Point(double x, double y, double z) {
+        this.xyz=new Double3(x, y, z);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj instanceof Point other)
+            return this.xyz.equals(other.xyz);
+        return false;
+    }
+
+    @Override
+    public String toString()
+    {
+        return xyz.toString();
+    }
+
+    public Point add(Vector v)
+    {
+        return new Point(xyz.add(v.xyz));
+    }
+
+    Vector subtract(Point p){
+        return new Vector(p.xyz.subtract(this.xyz));
+    }
+
+    double distanceSquared(Point other) {
+        return (other.xyz.d1-xyz.d1)*(other.xyz.d1-xyz.d1) +
+                (other.xyz.d2-xyz.d2)*(other.xyz.d2-xyz.d2) +
+                (other.xyz.d3-xyz.d3)*(other.xyz.d3-xyz.d3);
+
+
+    }
+
+    double distance(Point other)
+    {
+        return Math.sqrt(distanceSquared(other));
+    }
+}
---
+++ src/primitives/Vector.java
@@ -0,0 +1,49 @@
+package primitives;
+
+public class Vector extends Point {
+    Vector(Double3 xyz) {
+        super(xyz);
+        if (xyz.equals(Double3.ZERO))
+            throw new IllegalArgumentException("Vector Zero not allowable");
+    }
+
+    public Vector(double x, double y, double z) {
+        super(x, y, z);
+        if (xyz.equals(Double3.ZERO))
+            throw new IllegalArgumentException("Vector Zero not allowable");
+    }
+
+    public Vector add(Vector other) {
+        return new Vector(xyz.add(other.xyz));
+    }
+
+    public Vector scale(double scalar){
+        return new Vector(xyz.scale(scalar));
+    }
+
+    public double dotProduct(Vector other)
+    {
+        return (xyz.d1*other.xyz.d1) +
+                (xyz.d2*other.xyz.d2) +
+                (xyz.d3*other.xyz.d3);
+    }
+
+    public Vector crossProduct(Vector other)
+    {
+        return new Vector((xyz.d2*other.xyz.d3) - (xyz.d3*other.xyz.d2),
+                (xyz.d3*other.xyz.d1)-(xyz.d1*other.xyz.d3),
+                (xyz.d1*other.xyz.d2)-(xyz.d2*other.xyz.d1));
+    }
+    public double lengthSquared()
+    {
+        return dotProduct(this);
+    }
+
+    public double length(){
+        return Math.sqrt(lengthSquared());
+    }
+
+    public Vector normalize(){
+        return scale(1/length());
+    }
+}
