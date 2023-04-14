4d3663644b116f9d2c6d7163d2720f9b9e17bcf7
אביטל כהנא <avkahana@g.jct.ac.il>
2023-04-14 17:11:33 +0300

    cylinder normal

--- JUint/unittest/geometries/CylinderTest.java
+++ JUint/unittest/geometries/CylinderTest.java
@@ -17,40 +17,55 @@ class CylinderTest {
     /** Test method for {@link Cylinder#getNormal(Point)} (primitives.Point)}. */
 
     void testgetNormal() {
+        //The points to be checked
+        Point[] points ={new Point(0,0,0), new Point(0,0,2), new Point(0.2, 0.2, 2), new Point(-0.2, 0.1, 0), new Point(1,0,1)};
+        //The cylinder to be checked
+        Cylinder cy = new Cylinder(1, new Ray(new Point(0,0,0), new Vector(0,0,1)), 2);
+        Ray tmpRay = cy.getAxisRay();
+
         // ============ Equivalence Partitions Tests ==============
         // TC01: on the round surface
-        Cylinder cy = new Cylinder(1, new Ray(new Point(0,0,0), new Vector(0,0,1)), 2);
         // ensure there are no exceptions
-
-        Point[] points ={new Point()
-
-        for (int i=0; i<4; i++)
-        {
-            assertDoesNotThrow(() -> cy.getNormal(points[i]), "");
+        assertDoesNotThrow(() -> cy.getNormal(points[4]), "");
             // generate the test result
-            Vector result = cy.getNormal(points[i]);
+        Vector result = cy.getNormal(points[4]);
             // ensure |result| = 1
-            assertEquals(1, result.length(), 0.00000001, "Tube's normal is not a unit vector");
+        assertEquals(1, result.length(), 0.00000001, "Cylinder's normal is not a unit vector");
             // ensure the result is orthogonal to all the edges
-            assertTrue(isZero(result.dotProduct(new Vector(0,0,1))),
-                    "Tube's normal is not orthogonal to axis ray");
-
-        }
+        assertTrue(isZero(result.dotProduct(new Vector(0,0,1))),
+                    "Cylinder's normal is not orthogonal to axis ray");
         assertDoesNotThrow(() -> cy.getNormal(new Point(1, 2, 1)), "");
         // generate the test result
-        Vector result = cy.getNormal(new Point(1, 2, 3));
+        result = cy.getNormal(new Point(1, 2, 3));
         // ensure |result| = 1
-        assertEquals(1, result.length(), 0.00000001, "Tube's normal is not a unit vector");
+        assertEquals(1, result.length(), 0.00000001, "Cylinder's normal is not a unit vector");
         // ensure the result is orthogonal to all the edges
-        assertTrue(isZero(result.dotProduct(new Vector(0,0,1))),
-                "Tube's normal is not orthogonal to axis ray");
+        assertTrue(isZero(result.dotProduct(tmpRay.getDir())),
+                "Cylinder's normal is not orthogonal to axis ray");
 
         //on the bases (1 TC for each base)
-
+        assertDoesNotThrow(() -> cy.getNormal(points[2]), "");
+        assertDoesNotThrow(() -> cy.getNormal(points[3]), "");
+        for (int i =2; i<4 ; i++) {
+            result = cy.getNormal(points[i]);
+            assertEquals(1, result.length(), 0.00000001, "Cylinder's normal is not a unit vector");
+            Vector finalResult = result;
+            assertThrows(IllegalArgumentException.class, //
+                    () -> finalResult.crossProduct((tmpRay.getDir())),
+                    "Cylinder's normal isn't parallel to axis ray");
+        }
         // =============== Boundary Values Tests ==================
         // in the center of each base
-        assertDoesNotThrow(() -> cy.getNormal(new Point(1, 0, 0)), "");
-        assertTrue(isZero(result.dotProduct(new Vector(0,0,1))),
-                "Tube's normal is not orthogonal to axis ray");
+        assertDoesNotThrow(() -> cy.getNormal(points[0]), "");
+        assertDoesNotThrow(() -> cy.getNormal(points[1]), "");
+        for (int i =0; i<2 ; i++)
+        {
+            result = cy.getNormal(points[i]);
+            assertEquals(1, result.length(), 0.00000001, "Cylinder's normal is not a unit vector");
+            Vector finalResult = result;
+            assertThrows(IllegalArgumentException.class, //
+                    () -> finalResult.crossProduct((tmpRay.getDir())),
+                    "Cylinder's normal isn't parallel to axis ray");
+        }
     }
 }
